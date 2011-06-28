32d173b69a8a1105311ac8bb6812caec4c9df73f
Blake Elias <blakeelias@gmail.com>
2011-06-28 18:23:20 -0400

    Added details to send correct packet format

--- client.py
+++ client.py
@@ -1,13 +1,31 @@
 import socket
+import struct
 
 UDP_IP="127.0.0.1"
 UDP_PORT=5005
-MESSAGE="Hello, World!"
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+
+DATAGRAM_FORMAT = "ffffff"
+
+time  =  0.0
+left  = 25.0
+right = 30.0
+arm   =  5.0
+wrist =  2.0
+grip  =  3.0
+
+message = struct.pack(DATAGRAM_FORMAT,
+                      time,
+                      left,
+                      right,
+                      arm,
+                      wrist,
+                      grip)
+
+print "message:", message
 
 sock = socket.socket( socket.AF_INET, # Internet
                       socket.SOCK_DGRAM ) # UDP
-sock.sendto( MESSAGE, (UDP_IP, UDP_PORT) )
+sock.sendto( message, (UDP_IP, UDP_PORT) )
