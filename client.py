d45f74eda6c2bf3db81c027a21dbf49f6385e96e
Blake Elias <blakeelias@gmail.com>
2011-06-28 19:06:02 -0400

    Continuously send updates

--- client.py
+++ client.py
@@ -9,23 +9,27 @@ print("UDP target port:", UDP_PORT)
 
 DATAGRAM_FORMAT = "@ffffff"
 
-time  =  0.0
-left  = 25.0
-right = 30.0
-arm   =  5.0
-wrist =  2.0
-grip  =  3.0
+time = 0.0
 
-message = struct.pack(DATAGRAM_FORMAT,
-                      time,
-                      left,
-                      right,
-                      arm,
-                      wrist,
-                      grip)
-
-print("message:", message)
-
-sock = socket.socket( socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM ) # UDP
-sock.sendto( message, (UDP_IP, UDP_PORT) )
+while True:
+    left  = 100.0
+    right = 100.0
+    arm   =   5.0
+    wrist =   2.0
+    grip  =   3.0
+    
+    message = struct.pack(DATAGRAM_FORMAT,
+                          time,
+                          left,
+                          right,
+                          arm,
+                          wrist,
+                          grip)
+    
+    #print("message:", message)
+    
+    sock = socket.socket( socket.AF_INET, # Internet
+                          socket.SOCK_DGRAM ) # UDP
+    sock.sendto( message, (UDP_IP, UDP_PORT) )
+    time += 1
+    
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 UDP_IP="127.0.0.1"
-UDP_PORT=50003
+UDP_PORT=50001
 
 sock = socket.socket( socket.AF_INET, # Internet
                       socket.SOCK_DGRAM ) # UDP
