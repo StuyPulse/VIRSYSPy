2edb4416c02dcc52af1b2e931ffb3fb0fd92fca0
Blake Elias <blakeelias@gmail.com>
2011-06-29 15:19:24 -0400

    Moved network output to a daemon thread so other things can happen simultaneously

--- client.py
+++ client.py
@@ -1,35 +1,41 @@
+#!/usr/bin/python3.1
+
 import socket
 import struct
+import threading
 
-UDP_IP="127.0.0.1"
+
+UDP_IP="192.168.1.149"
 UDP_PORT=50001
-
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
-
 DATAGRAM_FORMAT = "=ffffff"
 
 time = 0.0
 
-while True:
-    left  =   110.0
-    right =   110.0
-    arm   =  -700.0
-    wrist =   200.0
-    grip  =     2.0
-    
-    message = struct.pack(DATAGRAM_FORMAT,
-                          time,
-                          left,
-                          right,
-                          arm,
-                          wrist,
-                          grip)
-    
-    #print("message:", message)
-    
-    sock = socket.socket( socket.AF_INET, # Internet
-                          socket.SOCK_DGRAM ) # UDP
-    sock.sendto( message, (UDP_IP, UDP_PORT) )
-    time += 1
-    
+class SendOutput(threading.Thread):
+    def run(self):
+        global time
+
+        while True:
+            left  =   110.0
+            right =   110.0
+            arm   =  -700.0
+            wrist =   200.0
+            grip  =     2.0
+            
+            message = struct.pack(DATAGRAM_FORMAT,
+                                  time,
+                                  left,
+                                  right,
+                                  arm,
+                                  wrist,
+                                  grip)
+            
+            sock = socket.socket( socket.AF_INET, # Internet
+                                  socket.SOCK_DGRAM ) # UDP
+            sock.sendto( message, (UDP_IP, UDP_PORT) )
+            time += 1
+            #print("message: ", message)
+
+s = SendOutput()
+s.daemon = True
+s.start()
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.1
+
+import client
+
+#print("hello")
+while True:
+    pass
