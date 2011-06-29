d80641493c4bc2a65b031d40b31a5e153a1a19c7
Blake Elias <blakeelias@gmail.com>
2011-06-29 18:43:54 -0400

    Reorganized output update structure

--- client.py
+++ client.py
@@ -7,31 +7,30 @@ from collections import namedtuple
 
 UDP_IP="192.168.1.149"
 UDP_PORT=50001
-
-
 output_fields = ("time", "left", "right", "arm", "wrist", "grip")
+
 datagram_format = "=" + "f" * len(output_fields)
 Output = namedtuple("Output", output_fields)
 
-output_values = {"time"  :    0.0,
-                 "left"  :  110.0,
-                 "right" :  110.0,
-                 "arm"   : -700.0,
-                 "wrist" :  200.0,
-                 "grip"  :    2.0}
-
-out_tuple = Output(**output_values)
+init_vals = dict([field, 0.0] for field in output_fields)
 
 class SendOutput(threading.Thread):
+    def __init__(self):
+        self.out_tuple = Output(**init_vals)
+        threading.Thread.__init__(self)
+
     def run(self):
-        global out_tuple
         while True:
-            message = struct.pack(datagram_format, *out_tuple)
+            message = struct.pack(datagram_format, *self.out_tuple)
             sock = socket.socket( socket.AF_INET, # Internet
                                   socket.SOCK_DGRAM ) # UDP
             sock.sendto( message, (UDP_IP, UDP_PORT) )
-            out_tuple = out_tuple._replace(time = out_tuple.time+1)
+            self.update_output(time = self.out_tuple.time+1)
             #print("message: ", message)
+
+    def update_output(self, **keywords):
+        self.out_tuple = self.out_tuple._replace(**keywords)
+
 
 s = SendOutput()
 s.daemon = True
--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import client
 
 #print("hello")
 while True:
-    pass
+    #client.s.update_output(grip=4.0)
+    client.s.update_output(**{"time"  :    0.0,
+                              "left"  :  110.0,
+                              "right" :  110.0,
+                              "arm"   : -700.0,
+                              "wrist" :  200.0,
+                              "grip"  :    3.0})
