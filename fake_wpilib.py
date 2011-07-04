09d366dbcc56cf75ca314e6fc9c4f4f73e87db49
Blake Elias <blakeelias@gmail.com>
2011-07-03 20:07:56 -0400

    Created Victor class

--- fake_wpilib.py
+++ fake_wpilib.py
@@ -1,1 +1,19 @@
-print("using fake_wpilib")
+import client
+
+class Victor:
+    def __init__(self, channel, slot = 7):
+        self.channel = channel
+        self.kMaxOut = 200
+    
+    def set(self, speed):
+        a = abs(speed)
+        if a > 1:
+            speed /= a
+        client.s.update_output("PWMOut", self.channel, speed * self.kMaxOut)
+        pass
+
+    def get(self):
+        pass
+
+    def pidWrite(self, output):
+        pass
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3.1
 
-import client
 try:
     import wpilib
 except:
     import fake_wpilib as wpilib
 
-while True:
-    client.s.update_output("PWMOut", 1, 110)
-    client.s.update_output("PWMOut", 2, 110)
-    client.s.update_output("PWMOut", 3, -700)
-    client.s.update_output("PWMOut", 4, 200)
-    client.s.update_output("PWMOut", 5, 3)
+v = wpilib.Victor(1)
+w = wpilib.Victor(2)
     
+while True:
+    v.set(0.7)
+    w.set(0.5)
