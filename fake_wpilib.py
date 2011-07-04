a97725faf052682fcca18ef6ce433c03784d5982
Blake Elias <blakeelias@gmail.com>
2011-07-03 20:37:25 -0400

    Made Victor's kMaxOut a class variable instead of object variable

--- fake_wpilib.py
+++ fake_wpilib.py
@@ -1,15 +1,18 @@
 import client
 
 class Victor:
+
+    # Largest amount of torque to output
+    kMaxOut = 200
+
     def __init__(self, channel, slot=4):
         self.channel = channel
-        self.kMaxOut = 200
     
     def set(self, speed, syncGroup=0):
         a = abs(speed)
         if a > 1:
             speed /= a
-        client.s.update_output("PWMOut", self.channel, speed * self.kMaxOut)
+        client.s.update_output("PWMOut", self.channel, speed * Victor.kMaxOut)
         pass
 
     def get(self):
