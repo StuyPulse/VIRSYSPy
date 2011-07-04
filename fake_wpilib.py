cdf29947b85d51ee5cc25d04916eb1e153f2aef3
Blake Elias <blakeelias@gmail.com>
2011-07-03 21:06:56 -0400

    Capitalized method names to match C++ library.

--- fake_wpilib.py
+++ fake_wpilib.py
@@ -8,17 +8,17 @@ class Victor:
     def __init__(self, channel, slot=4):
         self.channel = channel
     
-    def set(self, speed, syncGroup=0):
+    def Set(self, speed, syncGroup=0):
         a = abs(speed)
         if a > 1:
             speed /= a
         client.s.update_output("PWMOut", self.channel, speed * Victor.kMaxOut)
         pass
 
-    def get(self):
+    def Get(self):
         pass
 
-    def pidWrite(self, output):
+    def PIDWrite(self, output):
         pass
 
 Jaguar = Victor    # They basically do the same thing
