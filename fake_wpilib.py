5ab2af6e875325408fd382e6252027d0ed29a69a
Blake Elias <blakeelias@gmail.com>
2011-07-05 10:16:59 -0400

    Config file for motor torques.

--- fake_wpilib.py
+++ fake_wpilib.py
@@ -1,18 +1,28 @@
 import client
+import configparser
+
+torque_cfg = configparser.ConfigParser()
+torque_cfg.read("motorTorques.conf")
+
+torques = {}
+for motor in torque_cfg.options("Torques"):
+    torques[motor] = torque_cfg.getfloat("Torques", motor)
+print(torques)
 
 class Victor:
 
-    # Largest amount of torque to output
-    kMaxOut = 200
+    out_type = "PWMOut"
 
     def __init__(self, channel, slot=4):
         self.channel = channel
+        name = client.ports[Victor.out_type][self.channel]
+        self.kMaxOut = torques[name]
     
     def Set(self, speed, syncGroup=0):
         a = abs(speed)
         if a > 1:
             speed /= a
-        client.s.update_output("PWMOut", self.channel, speed * Victor.kMaxOut)
+        client.s.update_output(Victor.out_type, self.channel, speed * self.kMaxOut)
         pass
 
     def Get(self):
--- main.py
+++ main.py
@@ -7,7 +7,9 @@ except:
 
 v = wpilib.Jaguar(1)
 w = wpilib.Jaguar(2)
+arm_motor = wpilib.Jaguar(3)
     
 while True:
     v.Set(1)
     w.Set(0.6)
+    arm_motor.Set(-1)
