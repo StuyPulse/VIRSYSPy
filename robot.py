ea1a7a44f3a8018d604472719ffbd836b94726fc
Blake Elias <blakeelias@gmail.com>
2011-07-10 00:20:24 -0400

    Added WPILib robot classes: RobotBase, SimpleRobot, IterativeRobot.  Hard coded to run autonomous.

---
+++ robot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.1
+
+try:
+    import wpilib
+except:
+    import fake_wpilib as wpilib
+
+class MyRobot(wpilib.SimpleRobot):
+    def __init__(self):
+        self.v = wpilib.Jaguar(1)
+        self.w = wpilib.Jaguar(2)
+        self.arm_motor = wpilib.Jaguar(3)
+        self.arm_gyro = wpilib.Gyro(1)
+        self.left_enc = wpilib.Encoder(6, 5)
+        self.right_enc = wpilib.Encoder(8, 7)
+
+    def Autonomous(self):
+        while self.IsAutonomous() and self.IsEnabled():
+            self.v.Set(1)
+            self.w.Set(1)
+            #self.arm_motor.Set(-1)
+            wpilib.Timer.Wait(1)
+            self.v.Set(-1)
+            self.w.Set(-1)
+            #self.arm_motor.Set(-0.3)
+            wpilib.Timer.Wait(1)
+
+def run():
+    robot = MyRobot()
+    robot.StartCompetition()
+
+if __name__ == "__main__":
+    run()
